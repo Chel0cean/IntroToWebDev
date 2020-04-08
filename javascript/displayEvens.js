66a8e2c263d612ba2bb387f0a914383915f7909e
Chel0cean <chelsea.anna.miller@gmail.com>
2020-04-08 03:13:08 -0400

    update

--- javascript/displayEvens.js
+++ javascript/displayEvens.js
@@ -1,10 +1,10 @@
-/*bugs in code:
-When there are no evens to present the "please re-enter" statement does not get reset (run once with empty fields then run 1,5,2).
-Even Values need to be cleared and refilled each time the calculate button is pressed. 
-*/
 
 var evenNumbers = [];
 function calculate() {
+  evenNumbers.length = 0;
+  document.getElementById("inv").innerHTML = "";
+  document.getElementById("tables").innerHTML = "";
+
 
   var num1 =
   Number(document.getElementById("first-number").value);
@@ -17,20 +17,33 @@ function calculate() {
     console.log("step: " + step);
     document.getElementById("results").style.display = "table";
 
+
+
+
     for (var counter = num1; counter <= num2; counter += step) {
+      if (num1 =="" || num2 == "" || step == "" || isNaN(num1) || isNaN(num2) || isNaN(step) || step <= 0 || num2 < num1 && evenNumbers.length < 1) {
+        evenNumbers.push("filler");
+        document.getElementById("inv").innerHTML = "*All fields must be completed.<br>*Second Number must be greater than the first number. <br> *Only positive integers are valid.<br> *Please re-enter your numbers.";
+        document.getElementById("tables").innerHTML = "";
+        console.log("Please re-enter your numbers.");
+        return "Please re-enter your numbers.";
+        }
+
       if (counter % 2 === 0) {
       evenNumbers.push(counter);
       document.getElementById("inv").innerHTML = "";
       document.getElementById("tables").innerHTML += counter + "<br />";
       console.log(counter);
       }
-    if (num1 =="" || num2 == "" || step == "" || isNaN(num1) || isNaN(num2) || isNaN(step || step <= 0 || num2 < num1)) {
-      document.getElementById("inv").innerHTML = "*All fields must be completed.<br>*Second Number must be greater than the first number. <br> *Only positive integers are valid.<br> *Please re-enter your numbers.";
-      document.getElementById("tables").innerHTML = "";
-      console.log("Please re-enter your numbers.");
-      return "Please re-enter your numbers.";
+      
     }
-  }
 
 
+  while (evenNumbers.length < 1) {
+    document.getElementById("inv").innerHTML = "";
+    document.getElementById("tables").innerHTML = "No even numbers to display.";
+    break;
     }
+
+
+}
